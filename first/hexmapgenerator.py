cd1c480d1f1524189056cdb9b6117d5724c66bea
devincornell <devincornell@example.com>
2022-06-29 09:05:49 -0400

    working on random map generator

--- first/hexmapgenerator.py
+++ first/hexmapgenerator.py
@@ -7,6 +7,7 @@ def random_map(map_size: int, seed: int = 0):
     hmap = HexMap(map_size)
     
     all_positions = hmap.positions()
+    
     # random sampling for start, end, and blocks
     random.seed(seed)
     avoidset = set(random.sample(all_positions, len(all_positions) // 4))
@@ -14,6 +15,9 @@ def random_map(map_size: int, seed: int = 0):
     end = start
     while end == start:
         end = random.choice(list(all_positions))
+    
+    path = start.shortest_path_dfs(end, avoidset, 2*map_size)
+    
     return start, end, avoidset
 
 def run_test(map_size: int, num_runs: int):
