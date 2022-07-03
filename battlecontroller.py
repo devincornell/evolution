c761092c2ab2541a6017d579f0561cd2e4375753
devincornell <devincornell@example.com>
2022-07-02 20:07:12 -0400

    first try at making controller

---
+++ battlecontroller.py
@@ -0,0 +1,170 @@
+import collections
+import dataclasses
+from typing import List
+import mase
+ 
+ 
+import enum
+
+class ActionType(enum.Enum):
+    MOVE = enum.auto()
+    ATTACK = enum.auto()
+    CONSUME = enum.auto()
+    
+class Action:
+    pass
+
+@dataclasses.dataclass
+class MoveAction:
+    agent_id: mase.AgentID
+    new_pos: tuple
+    action_type: ActionType = ActionType.MOVE
+
+@dataclasses.dataclass
+class AttackAction:
+    agent_id: mase.AgentID
+    target_id: mase.AgentID
+    action_type: ActionType = ActionType.ATTACK
+    
+@dataclasses.dataclass
+class ConsumeAction:
+    agent_id: mase.AgentID
+    action_type: ActionType = ActionType.CONSUME
+
+class AgentAlreadyExistsInLocationError(Exception):
+    pass
+
+class LocationIsBlockedError(Exception):
+    pass
+
+class OutOfRangeError(Exception):
+    pass
+
+class CannotControlOtherTeamMemberError(Exception):
+    pass
+
+class OutOfMovesError(Exception):
+    pass
+
+class OutOfActionsError(Exception):
+    pass
+
+class NoOrbsAtLocationError(Exception):
+    pass
+
+class ActionNotRecognizedError(Exception):
+    pass
+
+@dataclasses.dataclass
+class BattleController:
+    '''This acts like the "Controller" for the game.'''
+    team_id: int
+    map: mase.HexMap
+    pool: mase.AgentStatePool
+    action_sequence: list = dataclasses.field(default_factory=list)
+    move_ct: collections.Counter = dataclasses.field(default_factory=collections.Counter)
+    action_ct: collections.Counter = dataclasses.field(default_factory=collections.Counter)
+    verbose: bool = False
+    
+    def move(self, agent_id: mase.AgentID, new_position: tuple):
+        '''Move the agent to a new position.'''
+        agent = self.pool.get_agent(agent_id)
+        current_pos = self.map.get_agent_pos(agent_id)
+        pos = mase.HexPosition(*new_position)
+        loc = self.map[pos]
+        
+        # do some error checking
+        if self.move_ct[agent_id] > 0:
+            raise OutOfMovesError('Agent {agent_id} has already moved this turn.')
+        elif self.team_id != agent.team_id:
+            raise CannotControlOtherTeamMemberError(f'You cannot move a member of another team. '
+                f'You are team {self.team_id} but agent {agent_id} is on team {agent.team_id}.')
+        elif len(loc.agents):
+            existing_agent = list(loc.agents)[0]
+            raise AgentAlreadyExistsInLocationError(f'Cannot move agent {agent_id}: '
+                f'agent {existing_agent} already exists at {pos}.')
+        elif loc.is_blocked:
+            raise LocationIsBlockedError(f'Agent {agent_id} cannot move to position {pos}: '
+                'it is blocked.')
+        elif current_pos.dist(pos) > self.pool.get_agent(agent_id).speed:
+            raise OutOfRangeError(f'Agent {agent_id} cannot move to position {pos} because '
+                f'it is distance {current_pos.dist(pos)} but agent speed is only '
+                f'{self.pool.get_agent(agent_id).speed}.')
+        
+        self.map.move_agent(agent_id, pos)
+        self.action_sequence.append(MoveAction(agent_id, pos))
+        self.move_ct[agent_id] += 1
+        if self.verbose: print(f'Agent {agent_id} moved {current_pos.dist(pos)} positions to {pos}.')
+        
+    def attack(self, agent_id: mase.AgentID, target_id: mase.AgentID):
+        '''Attack an agent of the opposite team.'''
+        agent = self.pool.get_agent(agent_id)
+        target_agent = self.pool.get_agent(target_id)
+        
+        pos = self.map.get_agent_pos(agent_id)
+        target_pos = self.map.get_agent_pos(target_id)
+        
+        # do some error checking
+        self.check_action(agent_id)
+        if pos.dist(target_pos) > 1:
+            raise OutOfRangeError('Agent {agent_id} cannot attack agent {target_id} because it is more than '
+                'one cell away.')
+
+        # apply attack
+        target_agent.health -= agent.attack
+        
+        if target_agent.health <= 0:
+            self.pool.remove_agent(target_agent.id)
+            self.map.remove_agent(target_agent.id)
+            if self.verbose: print(f'Agent {agent_id} killed {target_id}: {agent} vs {target_agent}.')
+        else:
+            if self.verbose: print(f'Agent {agent_id} attacked {target_id}, reducing '
+                f'health by {agent.attack} to {target_agent.health}.')
+        
+        self.action_ct[agent_id] += 1    
+        self.action_sequence.append(AttackAction(agent_id, target_id=target_id))
+        
+        
+    def consume(self, agent_id: mase.AgentID):
+        '''Agent can collect an orb from the location at it's current position.'''
+        loc = self.map.get_agent_loc(agent_id)
+        agent = self.pool.get_agent(agent_id)
+        
+        self.check_action(agent_id)
+        if not loc.state.orbs > 0:
+            raise NoOrbsAtLocationError(f'There are no orbs at the location of {agent_id}.')
+        
+        loc.state.orbs -= 1
+        agent.level += 1
+        agent.health += 1
+        
+        self.action_ct[agent_id] += 1    
+        self.action_sequence.append(ConsumeAction(agent_id))
+        if self.verbose: print(f'Agent {agent_id} consumed 1 orb at location {loc.state}.')
+        
+    def check_action(self, agent_id: mase.AgentID):
+        '''Make sure the agent can take another action.'''
+        agent = self.pool.get_agent(agent_id)
+        if self.action_ct[agent_id] > 0:
+            raise OutOfActionsError('Agent {agent_id} has already taken an action this turn.')
+        elif self.team_id != agent.team_id:
+            raise CannotControlOtherTeamMemberError(f'You cannot order a member of another team to attack. '
+                f'You are team {self.team_id} but agent {agent_id} is on team {agent.team_id}.')
+    
+    def get_actions(self):
+        return self.action_sequence.copy()
+    
+    def apply_actions(self, actions: List[Action]):
+        '''Apply the actions to this game state.'''
+        for action in actions:
+            if action.action_type is ActionType.MOVE:
+                self.move(action.agent_id, action.new_pos)
+            elif action.action_type is ActionType.ATTACK:
+                self.attack(action.agent_id, action.target_id)
+            elif action.action_type is ActionType.CONSUME:
+                self.consume(action.agent_id)
+            else:
+                raise ActionNotRecognizedError(f'The action {action} was not recognized by the controller.')
+    
+            
+            
--- battlegame.py
+++ battlegame.py
@@ -5,53 +5,50 @@ import enum
 import random
 import collections
 
+from .battlecontroller import BattleController
+#import battlecontroller
+
+@dataclasses.dataclass
 class BattleAgentState(mase.AgentState):
     id: int
-    team: int
+    team_id: int
     level: int = 1
-    speed: int = 1
+    health: int = 1
+    
+    @property
+    def speed(self):
+        '''Number of steps an agent can take in oen turn.'''
+        return self.level
+    
+    @property
+    def attack(self):
+        '''Health removed from other agent after attacking.'''
+        return self.level
+    
+    @property
+    def max_health(self):
+        return self.level
     
 @dataclasses.dataclass
-class BattleLocationState:
-    food: int
-    blocked: bool
+class BattleLocationState(mase.LocationState):
+    orbs: int
+    is_blocked: bool
     
-@dataclasses.dataclass
-class BattleController:
-    '''This acts like the "Controller" for the game.'''
 
 class BattleGame:
     '''A game implemented using mase.'''
-    def __init__(self, map_radius: int, num_start_warriors: int, *ai_players):
+    def __init__(self, ai_players: typing.List[typing.Callable], map_radius: int, blocked_ratio: float, food_ratio: float, num_start_warriors: int):
         '''Each player is a function or callable class to play the game.
         '''
         self.num_start_warriors = num_start_warriors
+        self.food_ratio = food_ratio
+        self.ai_players = list(ai_players)
         
-        self.next_id = 0
-        self.pool = mase.AgentStatePool()
-        self.map = self.generate_random_map(map_radius)
-        self.ai_players = list(enumerate(ai_players))
+        self.next_id: int = 0
+        self.pool: mase.AgentStatePool = mase.AgentStatePool()
+        self.map: mase.HexMap = self.generate_random_map(map_radius, blocked_ratio)
+        self.controller = BattleController()
         
-    def next_agent_id(self):
-        self.next_id += 1
-        return self.next_id - 1
-        
-    def setup(self):
-        '''Set up actual game.'''
-        locations = self.map.locations()
-        
-        for _ in range(self.num_start_warriors):
-            for player_id, player in self.ai_players:
-                
-                # create a new agent and add it to the pool
-                agent = BattleAgentState(self.next_agent_id(), player_id)
-                self.pool.add(agent.id, agent)
-                
-                # put agent in free, non-blocked location
-                loc = random.choice(locations)
-                while not loc['bs'].blocked and not len(loc.agents):
-                    loc = random.choice(locations)
-                self.map.add_agent(agent.id, loc.pos)
         
     def run(self):
         '''Start the simulation.'''
@@ -60,15 +57,65 @@ class BattleGame:
         
     @property
     def is_finished(self):
-        return len(collections.Counter([a.team for a in self.pool.values()]))
+        return len(set([a.team for a in self.pool.values()])) <= 1
         
     def step(self):
-        pass
+        for team_id, ai in enumerate(self.ai_players):
+            new_map = self.map.deepcopy()
+            new_pool = self.pool.deepcopy()
+            ai_controller = BattleController(team_id, new_map, new_pool)
+            
+            # actually execute ai
+            ai(team_id, new_map, new_pool, ai_controller)
+            
+            # get user actions and apply them to real map and pool
+            game_controller = BattleController(team_id, self.map, self.pool)
+            game_controller.apply_actions(ai_controller.get_actions())
+            
+            
+    
+    ################### Game Setup ###################
+    def setup(self):
+        '''Set up actual game.'''
+        locations = self.map.locations()
+        
+        # set up agents
+        for _ in range(self.num_start_warriors):
+            for player_id, player in enumerate(self.ai_players):
+                
+                # create a new agent and add it to the pool
+                agent = BattleAgentState(self.next_agent_id(), player_id)
+                self.pool.add(agent.id, agent)
+                
+                # put agent in free, non-blocked location
+                loc = random.choice(locations)
+                while not loc.state.is_blocked and not len(loc.agents):
+                    loc = random.choice(locations)
+                self.map.add_agent(agent.id, loc.pos)
+                
+        # spread orbs
+        free_loc = set([loc for loc in self.map.locations if not loc.state.is_blocked])
+        for loc in random.choices(free_loc, int(len(free_loc)*self.food_ratio)):
+            loc.state.orbs
+    
+    def next_agent_id(self):
+        self.next_id += 1
+        return self.next_id - 1
     
     @staticmethod
-    def generate_random_map(self, map_radius: int) -> mase.HexMap:
+    def generate_random_map(self, map_radius: int, blocked_ratio: float) -> mase.HexMap:
         '''Generates a random map according to some conditions.'''
-        default_loc_state = {'bs': BattleLocationState(0, False)}
-        game_map = mase.HexMap(map_radius, default_loc_state=default_loc_state)
+        game_map = mase.HexMap(map_radius, default_state = BattleLocationState)
+        
+        # block off some areas of the map
+        blocked_pos = random.choices(game_map.positions, int(len(game_map)*blocked_ratio))
+        for pos in blocked_pos:
+            game_map[pos].state.is_blocked = True
+        
         return game_map
         
+if __name__ == '__main__':
+    def example_ai(team_id: int, game_map: mase.HexMap, agents: mase.AgentStatePool):
+        print(f'Starting team {team_id} turn!')
+    
+    game = BattleGame()
