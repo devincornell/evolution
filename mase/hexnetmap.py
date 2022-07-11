f8419eb029357f848a50fb093af7246034b8359c
devincornell <devincornell@example.com>
2022-07-11 15:44:16 -0400

    trying to use agents instead of just ids

--- mase/hexnetmap.py
+++ mase/hexnetmap.py
@@ -3,13 +3,15 @@ import itertools
 import typing
 import igraph
 from .position import PyHexPosition
-from .location import Location, LocationState
+from .location import Locations, Location, LocationState
 from .errors import *
-from .agentid import AgentID
+#from .agentid import AgentID
+from .agent import Agent
 
 class HexNetMap:
     posmap: typing.Dict[PyHexPosition, igraph.Vertex]
-    agent_pos: typing.Dict[AgentID, PyHexPosition]
+    agent_pos: typing.Dict[Agent, PyHexPosition]
+
     def __init__(self, radius: int, default_state: LocationState = None):
         self.radius = radius
         self.agent_pos = dict()
@@ -37,9 +39,9 @@ class HexNetMap:
     def __getitem__(self, pos: PyHexPosition) -> Location:
         return self.location(pos)
 
-    def __contains__(self, agent_id: AgentID) -> bool:
+    def __contains__(self, agent: Agent) -> bool:
         '''Check if the agent is on the map.'''
-        return agent_id in self.agent_pos
+        return agent in self.agent_pos
     
     def __iter__(self) -> iter:
         return (v['loc'] for v in self.graph.vs)
@@ -50,7 +52,10 @@ class HexNetMap:
     def __contains__(self, pos: PyHexPosition) -> bool:
         return pos in self.posmap
     
-    ############################# Network Vertices #############################    
+    ############################# Vertices and Locations #############################    
+    def locations(self) -> Locations:
+        return Locations([v['loc'] for v in self.graph.vs])
+    
     def location(self, pos: PyHexPosition) -> Location:
         '''Get location at the desired position.'''
         return self.vertex(pos)['loc']
@@ -67,51 +72,51 @@ class HexNetMap:
         return self.vertex(PyHexPosition(*coords))
 
     ############################# Accessing Agent Locations #############################
-    def __contains__(self, agent_id: AgentID) -> bool:
+    def __contains__(self, agent: Agent) -> bool:
         '''Check if the agent is on the map.'''
-        return agent_id in self.agent_pos
+        return agent in self.agent_pos
     
     @property
-    def agent_ids(self) -> typing.List[AgentID]:
+    def agents(self) -> typing.List[Agent]:
         '''Get locations after filtering and sorting.'''
         return list(self.agent_pos.keys())
 
-    def get_agent_pos(self, agent_id: AgentID) -> PyHexPosition:
+    def get_agent_pos(self, agent: Agent) -> PyHexPosition:
         '''Get position of the provided agent.'''
         try:
-            return self.agent_pos[agent_id]
+            return self.agent_pos[agent]
         except KeyError:
             raise AgentDoesNotExistError('This agent does not exist on the map.')
 
-    def get_agent_loc(self, agent_id: AgentID) -> Location:
+    def get_agent_loc(self, agent: Agent) -> Location:
         '''Get the location object associated with the agent.'''
-        return self.location(self.get_agent_pos(agent_id))
+        return self.location(self.get_agent_pos(agent))
 
 
     ############################# Manipulating Agents #############################
 
-    def move_agent(self, agent_id: AgentID, new_pos: PyHexPosition):
+    def move_agent(self, agent: Agent, new_pos: PyHexPosition):
         '''Move the agent to a new location after checking rule.
         '''        
-        old_loc = self.get_agent_loc(agent_id)
+        old_loc = self.get_agent_loc(agent)
         new_loc = self[new_pos]
                         
-        old_loc.remove_agent(agent_id)
-        new_loc.add_agent(agent_id)
-        self.agent_pos[agent_id] = new_pos
+        old_loc.remove_agent(agent)
+        new_loc.add_agent(agent)
+        self.agent_pos[agent] = new_pos
 
-    def add_agent(self, agent_id: AgentID, pos: PyHexPosition):
+    def add_agent(self, agent: Agent, pos: PyHexPosition):
         '''Add the agent to the map.'''
-        if agent_id in self.agent_pos:
-            raise AgentExistsError(f'The agent "{agent_id}" already exists on this map.')
-        self.agent_pos[agent_id] = pos
-        self.location(pos).add_agent(agent_id)
+        if agent in self.agent_pos:
+            raise AgentExistsError(f'The agent "{agent}" already exists on this map.')
+        self.agent_pos[agent] = pos
+        self.location(pos).add_agent(agent)
         
-    def remove_agent(self, agent_id: AgentID):
+    def remove_agent(self, agent: Agent):
         '''Remove the agent form the map.'''
-        loc = self.get_agent_loc(agent_id)
-        loc.remove_agent(agent_id)
-        del self.agent_pos[agent_id]
+        loc = self.get_agent_loc(agent)
+        loc.remove_agent(agent)
+        del self.agent_pos[agent]
 
     ############################# Helpers #############################
 
